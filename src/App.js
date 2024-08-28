57ce312781166645469abaee495209bfed4c778f
Jhon1606 <barrosjhon936@gmail.com>
2024-08-28 14:53:10 -0500

    Primer cambio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje';
 
 function App() {
+  const hola = 'Hola'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {hola}
+      <Mensaje color='red' message="Hola mundo" />
+      <Mensaje color='yellow' message="desde módulo" />
     </div>
   );
 }
